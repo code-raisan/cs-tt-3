14a1473dd93d617207ea8f59e3ca3fad83a312e7
Raisan <yuri@rainyan.net>
2026-05-18 15:13:57 +0900

    [Edit] change to all green

--- js/tetris.js
+++ js/tetris.js
@@ -21,9 +21,8 @@ var shapes = [
     [ 0, 1, 0, 0,
       1, 1, 1 ]
 ];
-var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
-];
+var colors = new Array(7).fill("green");
+
 
 // creates a new 4x4 shape in global variable 'current'
 // 4x4 so as to cover the size when the shape is rotated
